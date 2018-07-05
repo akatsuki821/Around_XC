60562f17e62bb62ea3cb3a361dd14f944959a95c
xwang875 <xwang875@gatech.edu>
2018-07-05 19:36:26 -0400

    Token authentication implementation

--- service/main.go
+++ service/main.go
@@ -13,6 +13,9 @@ import (
 	"github.com/pborman/uuid"
     "context"
     "cloud.google.com/go/storage"
+    "github.com/auth0/go-jwt-middleware"
+    "github.com/dgrijalva/jwt-go"
+    "github.com/gorilla/mux"
 )
 
 const (
@@ -21,7 +24,7 @@ const (
 	DISTANCE = "200km"
 	PROJECT_ID = "aroundxc-209205"
 	BT_INSTANCE = "around-post"
-	ES_URL = "http://35.185.23.137:9200"
+	ES_URL = "http://35.196.146.20:9200"
     BUCKET_NAME = "post-images-209205"
 )
 
@@ -36,6 +39,8 @@ type Post struct {
 	Location Location `json:"location"`
     Url string `json:"url"`
 }
+
+var mySigningKey = []byte("secret")
 
 func main() {
 	//Create a client
@@ -69,8 +74,22 @@ func main() {
 		}
 	}
 	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
+    
+    r := mux.NewRouter()
+    
+    var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+        ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+            return mySigningKey, nil
+        },
+        SigningMethod: jwt.SigningMethodHS256,
+    })
+    
+    r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+    r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+    r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+    r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+    
+    http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -78,6 +97,10 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    
+    user := r.Context().Value("user")
+    claims := user.(*jwt.Token).Claims
+    username := claims.(jwt.MapClaims)["username"]
     // 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB
     r.ParseMultipartForm(32<<20)
     
@@ -88,7 +111,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	//Parse from body of request to get a json object
 
     p := &Post{
-        User: "1111",
+        User: username.(string),
         Message: r.FormValue("message"),
         Location: Location{
             Lat: lat,
